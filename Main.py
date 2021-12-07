d450ff6497b95b4ed59f523acefdd48e0e9fa7f4
every3743 <every3743@gmail.com>
2021-12-07 22:37:05 +0900

    siteSelect 삭제

--- Main.py
+++ Main.py
@@ -4,9 +4,10 @@
 
 # tkinter 임포트 합니다.
 import tkinter
+import tkinter.ttk # 이거 왜 추가해야되는 이유는 모르겠다. 아마도 콤보박스 관련 라이브러리라서 그런가
 
 
-window = tkinter.Tk() # 선언합니다.
+window = tkinter.Tk() # Tk()이용하여 윈도우 창을 생성합니다.
 window.title("동영상 다운로더") # 제목 설정합니다.
 
 # 너비, 높이, x, y의 좌표 선언합니다.
@@ -26,7 +27,7 @@ window.resizable(False, False)
 window.configure(bg = "#79579e")
 
 # 라벨 추가
-label = tkinter.Label(window, text = "동영상 다운로더", width = 100, height = 5, fg="#ffffff", font = ("DotumChe", 30))
+label = tkinter.Label(window, text = "동영상 다운로더", width = 100, height = 5, fg = "#ffffff", font = ("DotumChe", 30))
 label.configure(bg = "#79579e")
 label.pack()
 
@@ -38,15 +39,51 @@ def countUP():
     count += 1
     label.config(text = str(count))
 
+def self_window():
+    self_window = tkinter.Tk()
+    self_window.title("프로젝트 소개")
+    w = 500
+    h = 500
+    self_window.geometry('%dx%d+%d+%d' %(w, h, 500, 500))
+    self_window.resizable(False, False)
+    self_window.configure(bg = "#79579e")
+
+def select_window():
+    window = tkinter.Tk()
+    window.title("사이트 선택")
+    w = 900
+    h = 500
+    window.geometry('%dx%d+%d+%d' %(w, h, 500, 500))
+    window.resizable(False, False)
+    window.configure(bg = "#79579e")
+
+    self_label = tkinter.Label(window, text = "사이트 선택", width = 100, height = 5, fg = "#ffffff", font = ("DotumChe", 30))
+    self_label.configure(bg = "#79579e")
+    self_label.pack()
+
+    # 여기서 판다스로 엑셀 파일 읽어불러오기 로직넣는 부분
+    values=["네이버", "다음", "유튜브"]
+
+    # end
+
+    combobox=tkinter.ttk.Combobox(window, values = values)
+    combobox.set("유튜브")
+    combobox.pack()
+
+    button2 = tkinter.Button(window, text = "이동", overrelief = "flat", width = 100, height = 5, repeatdelay = 1000, repeatinterval = 100)
+    button2.configure(bg = "#ffffff")
+    button2.pack(padx = 50, pady = 100)
+    button2.pack()
+
 label = tkinter.Label(window, text = "0")
 label.pack()
 
-button = tkinter.Button(window, text = "사이트 선택", overrelief = "flat", width = 100, height = 5, command = countUP, repeatdelay = 1000, repeatinterval = 100)
-button.configure(bg="#ffffff")
+button = tkinter.Button(window, text = "사이트 선택", overrelief = "flat", width = 100, height = 5, command = select_window, repeatdelay = 1000, repeatinterval = 100)
+button.configure(bg = "#ffffff")
 button.pack(padx = 30, pady = 30)
 button.pack()
-
-button1 = tkinter.Button(window, text = "프로젝트 소개", overrelief = "flat", width = 100, height = 5, repeatdelay = 1000, repeatinterval = 100)
+ 
+button1 = tkinter.Button(text = "프로젝트 소개", overrelief = "flat", width = 100, height = 5, command = self_window, repeatdelay = 1000, repeatinterval = 100)
 button1.configure(bg="#ffffff")
 button1.pack(padx = 30, pady = 30)
 button1.pack()
