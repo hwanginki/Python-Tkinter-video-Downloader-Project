29639620af30b3cd5f58321c4132fd0028c6c529
every3743 <every3743@gmail.com>
2021-12-07 23:47:00 +0900

    라벨, 콤보박스, 기입창 코드 구현 추가

--- Main.py
+++ Main.py
@@ -6,7 +6,6 @@
 import tkinter
 import tkinter.ttk # 이거 왜 추가해야되는 이유는 모르겠다. 아마도 콤보박스 관련 라이브러리라서 그런가
 
-
 window = tkinter.Tk() # Tk()이용하여 윈도우 창을 생성합니다.
 window.title("동영상 다운로더") # 제목 설정합니다.
 
@@ -62,18 +61,73 @@ def select_window():
     self_label.pack()
 
     # 여기서 판다스로 엑셀 파일 읽어불러오기 로직넣는 부분
-    values=["네이버", "다음", "유튜브"]
-
+    values = ["네이버", "다음", "유튜브"]
     # end
 
     combobox=tkinter.ttk.Combobox(window, values = values)
     combobox.set("유튜브")
     combobox.pack()
 
-    button2 = tkinter.Button(window, text = "이동", overrelief = "flat", width = 100, height = 5, repeatdelay = 1000, repeatinterval = 100)
-    button2.configure(bg = "#ffffff")
+    button2 = tkinter.Button(window, text = "이동", overrelief = "flat", width = 100, height = 5, command = downlode_window, repeatdelay = 1000, repeatinterval = 100)
+    button2.configure(bg = "#c4df9b")
     button2.pack(padx = 50, pady = 100)
     button2.pack()
+
+def downlode_window():
+    window = tkinter.Tk()
+    window.title("다운로드")
+    w = 900
+    h = 500
+    window.geometry('%dx%d+%d+%d' %(w, h, 500, 500))
+    window.resizable(False, False)
+    window.configure(bg = "#79579e")
+    
+    # 콤보박스에서 값을 선택하면 그 라벨이 수정할 수 있도록 고칠 것
+    downlode_label = tkinter.Label(window, text = "유튜브", width = 100, height = 4, fg = "#ffffff", font = ("DotumChe", 30))
+    downlode_label.configure(bg = "#79579e")
+    downlode_label.pack()
+
+    # place() 위젯 배치에 x, y축 설정하여 사용합니다.
+    downlode_label = tkinter.Label(window, text = "동영상 UTL", width = 10, height = 5, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_label.configure(bg = "#79579e")
+    downlode_label.place(x = 50, y = 90)
+    
+    downlode_label = tkinter.Label(window, text = "동영상 품질", width = 10, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_label.configure(bg = "#79579e")
+    downlode_label.place(x = 50, y = 200)
+    
+    downlode_label = tkinter.Label(window, text = "저장경로", width = 10, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_label.configure(bg = "#79579e")
+    downlode_label.place(x = 50, y = 265)
+
+    # 입력하고 엔터하면 라벨에서 출력하는 로직
+    def calc(event):
+        label.config(text = "결과 = " + str(eval(entry.get())))
+
+    entry = tkinter.Entry(window, width = 70)
+    # bind() 이용해 함수를 쓰면 될 듯?!
+    entry.bind("<Return>", calc)
+    entry.place(x = 200, y = 135)
+
+    label = tkinter.Label(window)
+    label.place(x = 200, y = 100)
+
+    entry = tkinter.Entry(window, width = 70)
+    entry.place(x = 200, y = 270)
+    
+    # 품질 콤보박스 값 추가
+    quality_lists = ["2160p(4K)", "1440p(HD)", "1080p(HD)", "720p", "480p", "360p", "240p", "144p"]
+    combobox = tkinter.ttk.Combobox(window, values = quality_lists, width = 80)
+    combobox.set("품질 선택")
+    combobox.place(x = 200, y = 200)
+
+    button3 = tkinter.Button(window, text = "다운로드", overrelief = "flat", width = 100, height = 3, repeatdelay = 1000, repeatinterval = 100)
+    button3.configure(bg = "#c4df9b")
+    button3.place(x = 110, y = 350)
+
+    downlode_label = tkinter.Label(window, text = "다운로드 횟수 : ", width = 20, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_label.configure(bg = "#79579e")
+    downlode_label.place(x = 150, y = 430)
 
 label = tkinter.Label(window, text = "0")
 label.pack()
@@ -83,7 +137,7 @@ button.configure(bg = "#ffffff")
 button.pack(padx = 30, pady = 30)
 button.pack()
  
-button1 = tkinter.Button(text = "프로젝트 소개", overrelief = "flat", width = 100, height = 5, command = self_window, repeatdelay = 1000, repeatinterval = 100)
+button1 = tkinter.Button(text = "프로젝트 소개", overrelief = "flat", width = 100, height = 5, command = downlode_window, repeatdelay = 1000, repeatinterval = 100)
 button1.configure(bg="#ffffff")
 button1.pack(padx = 30, pady = 30)
 button1.pack()
