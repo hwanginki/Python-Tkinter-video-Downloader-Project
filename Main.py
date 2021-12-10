f19f83a27e8aca373f77ed64a6bb02c4d6310421
every3743 <every3743@gmail.com>
2021-12-10 11:09:28 +0900

    이미지로고 추가 및 창 가운데 정렬, 프로그레스바(로딩) 재수정 커밋

--- Main.py
+++ Main.py
@@ -55,12 +55,12 @@ def select_window():
 def progress_function(stream, chunk, bytes_remaining):
     global select
     # 프로그레스바 길이는 600이니깐 여기서 알고리즘 다시 계산할 것
-    prog = round((1 - bytes_remaining / select.filesize) * 100, 1)
-    print("1_>>>>", bytes_remaining)
-    print("2_>>>>", select.filesize)
+    prog = round((1 - bytes_remaining / select.filesize) * 100)
     print(prog, "% 다운로드 중입니다...")
-    progress_bar.step(1)
+    progress_bar.step(prog)
     progress_bar.update()
+    downlode_progress.config(text = prog)
+    downlode_progress.update()
 
 # 동영상 다운로드 로직
 def downloadVideo():
@@ -114,7 +114,11 @@ def downlode_window():
     window.title("다운로드")
     w = 900
     h = 500
-    window.geometry('%dx%d+%d+%d' %(w, h, 0, 650))
+    sw = window.winfo_screenwidth()
+    sh = window.winfo_screenheight()
+    x = (sw - w) / 2
+    y = (sh - h) / 2
+    window.geometry('%dx%d+%d+%d' %(w, h, x, y))
     window.resizable(False, False)
     window.configure(bg = "#79579e")
 
@@ -132,7 +136,7 @@ def downlode_window():
 
     ytdEntryVar = tkinter.StringVar()  # 변수타입에 유의
     global ytdEntry
-    ytdEntry = tkinter.Entry(window, width=83, textvariable = ytdEntryVar)
+    ytdEntry = tkinter.Entry(window, width = 83, textvariable = ytdEntryVar)
     ytdEntry.place(x = 200, y = 130)
 
     downlode_label = tkinter.Label(window, text = "동영상 품질", width = 10, fg = "#ffffff", font = ("DotumChe", 15))
@@ -143,9 +147,25 @@ def downlode_window():
     downlode_label.configure(bg = "#79579e")
     downlode_label.place(x = 50, y = 265)
 
+    # 진행률 라벨
+    downlode_pgn = tkinter.Label(window, text="진행률", width=10, fg="#ffffff", font=("DotumChe", 15))
+    downlode_pgn.configure(bg="#79579e")
+    downlode_pgn.place(x=50, y=310)
+
+    # 진행률 라벨
+    global downlode_progress
+    downlode_progress = tkinter.Label(window, text = "0", width = 3, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_progress.configure(bg = "#79579e")
+    downlode_progress.place(x = 730, y = 310)
+
+    # 진행률 % 라벨
+    downlode_progressPer = tkinter.Label(window, text = " %", width = 2, fg = "#ffffff", font = ("DotumChe", 15))
+    downlode_progressPer.configure(bg = "#79579e")
+    downlode_progressPer.place(x = 763, y = 310)
+
     # global 변수 사용합니다.
     global locationError
-    locationError = tkinter.Label(window, text="", width = 45, fg="black", font=("jost", 15))
+    locationError = tkinter.Label(window, text = "", width = 45, fg = "black", font=("jost", 15))
     locationError.place(x=200, y=270)
 
     # 품질 콤보박스 값 추가
@@ -163,7 +183,7 @@ def downlode_window():
 
     # 프로그레스바 로직
     global progress_bar
-    progress_bar = tkinter.ttk.Progressbar(window, maximum = 100, length = 600,  mode = 'determinate')
+    progress_bar = tkinter.ttk.Progressbar(window, maximum = 100, length = 500,  mode = 'determinate')
     progress_bar.place(x = 200, y = 310)
     
     # 다운로드 버튼
@@ -182,13 +202,14 @@ window.title("동영상 다운로더") # 제목 설정합니다.
 
 # 너비, 높이, x, y의 좌표 선언합니다.
 w = 900
-h = 600
-# sw = winfo_screenwidth()
-# sh = winfo_screenheight()
-# x = (sw - w) / 2
-# y = (sh - h) / 2
+h = 500
+sw = window.winfo_screenwidth()
+sh = window.winfo_screenheight()
+x = (sw - w) / 2
+y = (sh - h) / 2
 
-window.geometry('%dx%d+%d+%d' %(w, h, 0, 0)) # geometry("너비x높이+x좌표+y좌표")
+# 화면 가운데 정렬
+window.geometry('%dx%d+%d+%d' %(w, h, x, y)) # geometry("너비x높이+x좌표+y좌표")
 window.resizable(False, False)
 # resizable(상하, 좌우)을 이용하여 윈도우 창의 창 크기 조절 가능 여부 설정,
 # True로 설정하는 경우는 윈도우 창의 크기를 조절할 수 있습니다.
@@ -196,21 +217,28 @@ window.resizable(False, False)
 # 배경색 설정
 window.configure(bg = "#79579e")
 
-# 라벨 추가
-label = tkinter.Label(window, text = "동영상 다운로더", width = 100, height = 5, fg = "#ffffff", font = ("DotumChe", 30))
-label.configure(bg = "#79579e")
-label.pack()
+# 이미지 로고 추가
+image = tkinter.PhotoImage(file = "Youtube_logo.png")
+image_label = tkinter.Label(window, image = image)
+image_label.pack()
+image_label.place(x = 350, y = 80)
 
+# 라벨 추가
+# label = tkinter.Label(window, text = "동영상 다운로더", width = 100, height = 5, fg = "#ffffff", font = ("DotumChe", 30))
+# label.configure(bg = "#79579e")
+# label.pack()
+
+# 사이트 선택 버튼 추가
 button = tkinter.Button(window, text = "사이트 선택", overrelief = "flat", width = 100, height = 5,
                         command = select_window, repeatdelay = 1000, repeatinterval = 100)
 button.configure(bg = "#ffffff")
-button.pack(padx = 30, pady = 30)
 button.pack()
+button.place(x = 100, y = 230)
 
+# 프로젝트 소개 버튼 추가
 button1 = tkinter.Button(text = "프로젝트 소개", overrelief = "flat", width = 100, height = 5, command = downlode_window)
-button1.configure(bg="#ffffff")
-button1.pack(padx = 30, pady = 30)
-button1.pack()
+button1.configure(bg = "#ffffff")
+button1.place(x = 100, y = 350)
 
 # 윈도우 창 뜨는 함수
 window.mainloop()
